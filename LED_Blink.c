92dd427c36847a3c2e26dd75a1da85d5f066a033
Meghana <53141586+lathamithu@users.noreply.github.com>
2020-05-31 17:29:45 +0530

    Create LED_Blink.c

---
+++ LED_Blink.c
@@ -0,0 +1,19 @@
+#include<LPC214X.h>
+void delay();
+int main()
+{
+  IODIR1 = 0x00ff0000;
+  while(1)
+  {
+    IOSET1 = 0x00aa0000;
+    delay();
+    IOCLR1 = 0x00ff0000;
+    delay();
+  }
+}
+void delay()
+{
+  int i,j;
+  for(i=0;i<1000;i++)
+  for(j=0;j<1000;j++);
+}
